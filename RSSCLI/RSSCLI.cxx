21cfa72ffc8c890888e35af6120cba1ce3592ea9
Yi Gao <gaoyi@uab.edu>
2013-10-18 10:36:47 -0500

    ENH: now compiles. next test

--- RSSCLI/RSSCLI.cxx
+++ RSSCLI/RSSCLI.cxx
@@ -1,10 +1,13 @@
+#include "RSSCLICLP.h"
+
+#include <iostream>
+
+#include "SFLSRobustStatSegmentor3DLabelMap_single.h"
+
+#include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 
-#include "itkSmoothingRecursiveGaussianImageFilter.h"
-
-#include "itkPluginUtilities.h"
-
-#include "RSSCLICLP.h"
+#include "labelMapPreprocessor.h"
 
 // Use an anonymous namespace to keep class types and function names
 // from colliding when module is used as shared object module.  Every
@@ -14,99 +17,209 @@
 namespace
 {
 
-template <class T>
-int DoIt( int argc, char * argv[], T )
+template <typename TPixel>
+itk::Image<short, 3>::Pointer
+getFinalMask(typename itk::Image<TPixel, 3>::Pointer img, unsigned char l, TPixel thod = 0)
 {
-  PARSE_ARGS;
+  typedef itk::Image<short, 3> MaskType;
 
-  typedef    T InputPixelType;
-  typedef    T OutputPixelType;
+  MaskType::SizeType size = img->GetLargestPossibleRegion().GetSize();
 
-  typedef itk::Image<InputPixelType,  3> InputImageType;
-  typedef itk::Image<OutputPixelType, 3> OutputImageType;
+  long nx = size[0];
+  long ny = size[1];
+  long nz = size[2];
 
-  typedef itk::ImageFileReader<InputImageType>  ReaderType;
-  typedef itk::ImageFileWriter<OutputImageType> WriterType;
+  MaskType::Pointer   mask = MaskType::New();
+  MaskType::IndexType start = {{0, 0, 0}};
 
-  typedef itk::SmoothingRecursiveGaussianImageFilter<
-    InputImageType, OutputImageType>  FilterType;
+  MaskType::RegionType region;
+  region.SetSize( size );
+  region.SetIndex( start );
 
-  typename ReaderType::Pointer reader = ReaderType::New();
+  mask->SetRegions( region );
 
-  reader->SetFileName( inputVolume.c_str() );
+  mask->SetSpacing(img->GetSpacing() );
+  mask->SetOrigin(img->GetOrigin() );
 
-  typename FilterType::Pointer filter = FilterType::New();
-  filter->SetInput( reader->GetOutput() );
-  filter->SetSigma( sigma );
+  mask->Allocate();
+  mask->FillBuffer(0);
+  for( long ix = 0; ix < nx; ++ix )
+    {
+    for( long iy = 0; iy < ny; ++iy )
+      {
+      for( long iz = 0; iz < nz; ++iz )
+        {
+        MaskType::IndexType idx = {{ix, iy, iz}};
+        TPixel              v = img->GetPixel(idx);
 
-  typename WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName( outputVolume.c_str() );
-  writer->SetInput( filter->GetOutput() );
-  writer->SetUseCompression(1);
-  writer->Update();
+        mask->SetPixel(idx, v <= thod ? l : 0);
+        }
+      }
+    }
 
-  return EXIT_SUCCESS;
+  return mask;
 }
 
 } // end of anonymous namespace
 
-int main( int argc, char * argv[] )
+
+
+int main(int argc, char* * argv)
 {
   PARSE_ARGS;
 
-  itk::ImageIOBase::IOPixelType     pixelType;
-  itk::ImageIOBase::IOComponentType componentType;
+  typedef short                                         PixelType;
+  typedef CSFLSRobustStatSegmentor3DLabelMap<PixelType> SFLSRobustStatSegmentor3DLabelMap_c;
+
+  // read input image
+  typedef SFLSRobustStatSegmentor3DLabelMap_c::TImage Image_t;
+
+  typedef itk::ImageFileReader<Image_t> ImageReaderType;
+  ImageReaderType::Pointer reader = ImageReaderType::New();
+  reader->SetFileName(originalImageFileName.c_str() );
+  Image_t::Pointer img;
 
   try
     {
-    itk::GetImageType(inputVolume, pixelType, componentType);
+    reader->Update();
+    img = reader->GetOutput();
+    }
+  catch( itk::ExceptionObject & err )
+    {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    raise(SIGABRT);
+    }
 
-    // This filter handles all types on input, but only produces
-    // signed types
-    switch( componentType )
+  // read input label image
+  typedef SFLSRobustStatSegmentor3DLabelMap_c::TLabelImage LabelImage_t;
+
+  typedef itk::ImageFileReader<LabelImage_t> LabelImageReader_t;
+  LabelImageReader_t::Pointer readerLabel = LabelImageReader_t::New();
+  readerLabel->SetFileName(labelImageFileName.c_str() );
+  LabelImage_t::Pointer labelImg;
+
+  try
+    {
+    readerLabel->Update();
+    labelImg = readerLabel->GetOutput();
+    }
+  catch( itk::ExceptionObject & err )
+    {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    raise(SIGABRT);
+    }
+
+  // preprocess label map (labelImg, the naming is confusing.....)
+  LabelImage_t::Pointer newLabelMap = preprocessLabelMap<LabelImage_t::PixelType>(labelImg, labelValue);
+
+
+  typedef itk::ImageFileReader< LabelImage_t > RejctionMaskReader_t;
+  RejctionMaskReader_t::Pointer rejctionReader = RejctionMaskReader_t::New();
+
+  if ( bUseRejctionLabelImage )
+    {
+    std::cout << "Using rejection mask." << std::endl;
+
+    rejctionReader->SetFileName( rejectionlabelImageFileName.c_str() );
+
+    try
       {
-      case itk::ImageIOBase::UCHAR:
-        return DoIt( argc, argv, static_cast<unsigned char>(0) );
-        break;
-      case itk::ImageIOBase::CHAR:
-        return DoIt( argc, argv, static_cast<char>(0) );
-        break;
-      case itk::ImageIOBase::USHORT:
-        return DoIt( argc, argv, static_cast<unsigned short>(0) );
-        break;
-      case itk::ImageIOBase::SHORT:
-        return DoIt( argc, argv, static_cast<short>(0) );
-        break;
-      case itk::ImageIOBase::UINT:
-        return DoIt( argc, argv, static_cast<unsigned int>(0) );
-        break;
-      case itk::ImageIOBase::INT:
-        return DoIt( argc, argv, static_cast<int>(0) );
-        break;
-      case itk::ImageIOBase::ULONG:
-        return DoIt( argc, argv, static_cast<unsigned long>(0) );
-        break;
-      case itk::ImageIOBase::LONG:
-        return DoIt( argc, argv, static_cast<long>(0) );
-        break;
-      case itk::ImageIOBase::FLOAT:
-        return DoIt( argc, argv, static_cast<float>(0) );
-        break;
-      case itk::ImageIOBase::DOUBLE:
-        return DoIt( argc, argv, static_cast<double>(0) );
-        break;
-      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
-      default:
-        std::cout << "unknown component type" << std::endl;
-        break;
+      rejctionReader->Update();
+      }
+    catch ( itk::ExceptionObject &err)
+      {
+      std::cerr<< "ExceptionObject caught !" << std::endl; 
+      std::cerr<< err << std::endl; 
+      raise(SIGABRT);
       }
     }
 
-  catch( itk::ExceptionObject & excep )
+
+  // do seg
+  SFLSRobustStatSegmentor3DLabelMap_c seg;
+  seg.setImage(img);
+
+  seg.setNumIter(10000); // a large enough number, s.t. will not be stoped by this creteria.
+  seg.setMaxVolume(expectedVolume);
+  seg.setInputLabelImage(newLabelMap);
+
+  // seg.setNumIter(numOfIteration);
+  seg.setMaxRunningTime(maxRunningTime);
+
+  seg.setIntensityHomogeneity(intensityHomogeneity);
+  seg.setCurvatureWeight(curvatureWeight / 1.5);
+
+  if ( bUseRejctionLabelImage )
     {
-    std::cerr << argv[0] << ": exception caught !" << std::endl;
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
+    seg.setRejectionMask( rejctionReader->GetOutput() );
     }
+
+
+  seg.doSegmenation();
+
+//   typedef int PixelType;
+//   typedef itk::Image< PixelType, 3 > ImageType;
+
+//   typedef itk::ImageFileReader< ImageType > ImageReaderType;
+//   ImageReaderType::Pointer reader = ImageReaderType::New();
+//   reader->SetFileName(originalImageFileName.c_str());
+
+//   ImageType::Pointer img;
+
+//   try
+//     {
+//       reader->Update();
+//       img = reader->GetOutput();
+//     }
+//   catch ( itk::ExceptionObject &err)
+//     {
+//       std::cerr<< "ExceptionObject caught !" << std::endl;
+//       std::cerr<< err << std::endl;
+//       raise(SIGABRT);
+//     }
+
+// //   std::vector<std::vector<long> > seedListIJK;
+// //   seedsPreprocess<ImageType>(img, seed, seedListIJK);
+
+//   CSFLSRobustStatSegmentor3D< PixelType > seg;
+//   seg.setImage(img);
+
+//   seg.setSeeds(seedListIJK);
+//   seg.setMaxRunningTime(maxRunningTime);
+//   seg.setMaxVolume(expectedVolume);
+
+//   seg.setIntensityHomogeneity(intensityHomogeneity);
+
+//   //seg.setNumIter(numOfIteration);
+
+//   seg.setCurvatureWeight(curvatureWeight/2.0); // in the interface, it's 0~1 scale, internally, it's 0~0.5
+
+//   seg.doSegmenation();
+
+  typedef itk::Image<short, 3> MaskImageType;
+
+  MaskImageType::Pointer finalMask = getFinalMask<float>(seg.mp_phi, labelValue, 2.0);
+  finalMask->CopyInformation(img);
+
+  typedef itk::ImageFileWriter<MaskImageType> WriterType;
+  WriterType::Pointer outputWriter = WriterType::New();
+  outputWriter->SetFileName(segmentedImageFileName.c_str() );
+  outputWriter->SetInput(finalMask);
+  outputWriter->Update();
+
+  try
+    {
+    outputWriter->Update();
+    }
+  catch( itk::ExceptionObject & err )
+    {
+    std::cout << "ExceptionObject caught !" << std::endl;
+    std::cout << err << std::endl;
+    raise(SIGABRT);
+    }
+
   return EXIT_SUCCESS;
 }
+
